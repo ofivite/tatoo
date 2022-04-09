ba38f0ea8933440c29df136e5a2adedd3d6577b2
Oleg Filatov <20295221+yaourtpourtoi@users.noreply.github.com>
2022-04-09 13:28:20 +0200

    plotting multiple models

--- plot_roc.py
+++ plot_roc.py
@@ -13,17 +13,35 @@ from utils.plotting import derive_roc_curve
 
 @hydra.main(config_path='configs', config_name='plot_roc')
 def main(cfg: DictConfig) -> None:
-    path_to_artifacts = to_absolute_path(f'{cfg["path_to_mlflow"]}/{cfg["experiment_id"]}/{cfg["run_id"]}/artifacts')
     vs_type = cfg["vs_type"]
-
     figure_filename = f'{cfg["figure_filename"]}.pdf'
     fig, ax = plt.subplots(figsize=(10, 10))
     lw = 3
-
     tpr_grid = np.linspace(start=0., stop=1., num=100000, endpoint=True)
-    predictions = glob(f'{path_to_artifacts}/predictions/{cfg["dataset_name"]}/*/{vs_type}/*.h5')
-    (fpr_mean, fpr_std, auc_mean, auc_std), (fpr_deeptau_mean, fpr_deeptau_std, auc_deeptau_mean, auc_deeptau_std) = derive_roc_curve(predictions, cfg["n_samples_to_take"], tpr_grid)
 
+    for model_name, (experiment_id, run_id, linestyle, alpha) in cfg["models"].items():
+        path_to_artifacts = to_absolute_path(f'{cfg["path_to_mlflow"]}/{experiment_id}/{run_id}/artifacts')
+        predictions = glob(f'{path_to_artifacts}/predictions/{cfg["dataset_name"]}/*/{vs_type}/*.h5')
+        (fpr_mean, fpr_std, auc_mean, auc_std), (fpr_deeptau_mean, fpr_deeptau_std, auc_deeptau_mean, auc_deeptau_std) = derive_roc_curve(predictions, cfg["n_samples_to_take"], tpr_grid)
+        
+        plt.plot(
+            tpr_grid,
+            fpr_mean,
+            color="brown",
+            lw=lw,
+            linestyle=linestyle,
+            alpha=alpha,
+            label=f'{model_name}, AUC={auc_mean:.{cfg["auc_precision"][vs_type]}f} $\pm$ {auc_std:.{cfg["auc_precision"][vs_type]}f}',
+        )
+        plt.fill_between(
+            tpr_grid,
+            fpr_mean-fpr_std,
+            fpr_mean+fpr_std,
+            color="brown",
+            alpha=0.15
+        )
+
+    # will plot DeepTau roc curve taken from the last model's predictions
     plt.plot(
         tpr_grid,
         fpr_deeptau_mean,
@@ -31,21 +49,6 @@ def main(cfg: DictConfig) -> None:
         lw=lw,
         # linestyle='dashed',
         label=f'DeepTau, AUC={auc_deeptau_mean:.{cfg["auc_precision"][vs_type]}f} $\pm$ {auc_deeptau_std:.{cfg["auc_precision"][vs_type]}f}',
-    )
-    plt.plot(
-        tpr_grid,
-        fpr_mean,
-        color="brown",
-        lw=lw,
-        # linestyle='dashed',
-        label=f'{cfg["model_name"]}, AUC={auc_mean:.{cfg["auc_precision"][vs_type]}f} $\pm$ {auc_std:.{cfg["auc_precision"][vs_type]}f}',
-    )
-    plt.fill_between(
-        tpr_grid,
-        fpr_mean-fpr_std,
-        fpr_mean+fpr_std,
-        color="brown",
-        alpha=0.15
     )
     plt.fill_between(
         tpr_grid,
@@ -72,7 +75,7 @@ def main(cfg: DictConfig) -> None:
 
     # log to mlflow and delete intermediate file
     mlflow.set_tracking_uri(f'file://{to_absolute_path(cfg["path_to_mlflow"])}')
-    with mlflow.start_run(experiment_id=cfg["experiment_id"], run_id=cfg["run_id"]) as active_run:
+    with mlflow.start_run(experiment_id=experiment_id, run_id=run_id) as active_run:
         mlflow.log_artifact(figure_filename, 'plots')
     os.remove(figure_filename)
 
